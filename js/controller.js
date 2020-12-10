7ebdbc475df2107c55bb70fe815321848a2747db
Adirhagag <59648658+Adirhagag@users.noreply.github.com>
2020-12-10 19:59:46 +0200

    first upload

---
+++ js/controller.js
@@ -0,0 +1,244 @@
+'use strict';
+let gCanvas;
+let gCtx;
+let gRowsCount = 1;
+let gImgCounter = 0;
+
+
+let onInit = () => {
+  renderImgs();
+  getCanvas();
+  makePalteWhite();
+}
+
+
+// let renderImgs = (filter = 'funny') => {
+//   if (gImgCounter === 0) {
+//     var imgs = createImgs();
+//   }
+//   gImgCounter++
+//   let afterFilter = [];
+
+//   imgs.forEach(img => {
+//     if (img.keywords[0].includes(filter) || img.keywords[1].includes(filter)) {
+//       afterFilter.push(img);
+//     }
+//   });
+
+//   let strHTMLs = afterFilter.map(img => {
+//     return `<div class="img-wrapper"><img data-id="${img.id}" src="${img.url}" alt=""
+//     onclick="onImgClick('${img.url}', ${img.id})"></div>`;
+//   });
+//   console.log(afterFilter);
+
+//   document.querySelector('.imgs-container').innerHTML = strHTMLs.join('');
+// }
+
+let renderImgs = () => {
+  let imgs = createImgs();
+  let strHTMLs = imgs.map(img => {
+    return `<div class="img-wrapper"><img data-id="${img.id}" src="${img.url}" alt=""
+    onclick="onImgClick('${img.url}', ${img.id})"></div>`;
+  });
+  document.querySelector('.imgs-container').innerHTML = strHTMLs.join('');
+}
+
+
+let getCanvas = () => {
+  gCanvas = document.getElementById('meme-canvas');
+  gCtx = gCanvas.getContext('2d');
+}
+
+
+let onImgClick = (imgUrl, imgId) => {
+  updateGMemeId(imgId);
+
+  _displayImgToCanvas(imgUrl);
+  document.querySelector('main').style.display = 'none';
+  document.querySelector('.filter-imgs').style.display = 'none';
+  document.querySelector('.editor-container').style.display = 'flex';
+  resizeCanvas();
+  document.querySelector('.meme-txt input').focus();
+}
+
+
+let _displayImgToCanvas = imgUrl => {
+  let memeStat = getGMeme();
+
+  var img = new Image();
+  img.src = imgUrl;
+  img.onload = () => {
+    gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height);
+
+    if (gRowsCount === 0) return;
+    drawText(memeStat.lines[0].stroke, memeStat.lines[0].align, memeStat.lines[0].size, memeStat.lines[0].font, memeStat.lines[0].txt, memeStat.lines[0].color);
+
+    if (gRowsCount > 1) {
+      drawText(memeStat.lines[1].stroke, memeStat.lines[1].align, memeStat.lines[1].size, memeStat.lines[1].font, memeStat.lines[1].txt, memeStat.lines[1].color, 40, 450);
+    }
+  };
+
+}
+
+
+let onGetUserTxt = txt => {
+  let x = 40;
+  let y;
+  let memeStat = getGMeme();
+
+  displayAfterChange();
+  let lineIdx = memeStat.selectedLineIdx;
+
+  if (lineIdx === -1) return;
+  memeStat.lines[lineIdx].txt = txt;
+
+  if (lineIdx === 0) y = 40;
+  else if (lineIdx === 1) y = 450;
+
+  drawText(memeStat.lines[lineIdx].stroke, memeStat.lines[lineIdx].align, memeStat.lines[lineIdx].size, memeStat.lines[lineIdx].font, memeStat.lines[lineIdx].txt, memeStat.lines[lineIdx].color, x, y);
+}
+
+
+let drawText = (stroke = '#000000', txtAlign = 'Right', fontSize = 40, font = 'Impact', txt, color = '#ffffff', x = 40, y = 40) => {
+  if (!txt) return;
+
+  gCtx.beginPath();
+  gCtx.lineWidth = '2';
+  gCtx.font = `${fontSize}px ${font}`;
+  gCtx.fillStyle = color;
+  gCtx.strokeStyle = stroke;
+  gCtx.textAlign = txtAlign;
+  let txtWidth = gCtx.measureText(txt).width;
+
+  if (txtAlign === 'Right') x = (txtWidth + 7);
+  else if (txtAlign === 'right') x = (gCanvas.width - 7) - (txtWidth / txtWidth);
+  else if (txtAlign === 'center') x = (gCanvas.width / 2) - (txtWidth / txtWidth);
+
+  gCtx.fillText(txt, x, y);
+  gCtx.strokeText(txt, x, y);
+
+}
+
+
+let onAddLine = () => {
+  if (gRowsCount === 2) return;
+  let elInput = document.querySelector('.meme-txt input');
+  addLine();
+  elInput.value = '';
+  makePalteWhite();
+  gRowsCount === 0 ? drawSqure(5, 5, 'yellow') : drawSqure(5, gCanvas.height / 100 * 76, 'yellow');
+  gRowsCount++;
+  elInput.focus();
+}
+
+
+let drawSqure = (x = 5, y = 5, borderColor = 'black') => {
+  gCtx.beginPath();
+  gCtx.rect(x, y, 490, 100);
+  gCtx.closePath();
+  gCtx.strokeStyle = borderColor;
+  gCtx.stroke();
+}
+
+
+let onSwitchLine = () => {
+  let memeStat = getGMeme();
+  if (memeStat.lines.length === 1) return;
+
+  let elSelect = document.querySelector('select');
+  let elColor = document.querySelector('.inner-color');
+
+  if (memeStat.selectedLineIdx === 0) {
+    memeStat.selectedLineIdx = 1;
+    drawSqure(5, 380, 'yellow');
+    elSelect.value = memeStat.lines[1].font;
+    elColor.value = memeStat.lines[1].color;
+  } else if (memeStat.selectedLineIdx === 1) {
+    memeStat.selectedLineIdx = 0;
+    drawSqure(5, 5, 'yellow');
+    elSelect.value = memeStat.lines[0].font;
+    elColor.value = memeStat.lines[0].color;
+  }
+
+  let elInput = document.querySelector('.meme-txt input');
+  elInput.value = memeStat.lines[memeStat.selectedLineIdx].txt;
+  elInput.focus();
+}
+
+
+let onChangeFont = font => {
+  updateFont(font);
+  displayAfterChange();
+  document.querySelector('.meme-txt input').focus();
+}
+
+
+let onChangeTextColor = color => {
+  updateColor(color);
+  displayAfterChange();
+  document.querySelector('.meme-txt input').focus();
+}
+
+
+let makePalteWhite = () => {
+  document.querySelector('.inner-color').value = '#ffffff';
+}
+
+
+let onFontSizing = operator => {
+  updateFontSize(operator);
+  displayAfterChange();
+}
+
+
+let onFontAligment = dirName => {
+  updateFontAligment(dirName);
+  displayAfterChange();
+}
+
+
+let onDeleteLine = () => {
+  gRowsCount--;
+
+  let lineIdx = deleteLine();
+  let memeStat = getGMeme();
+  let lineTxt = lineIdx === undefined ? '' : memeStat.lines[lineIdx].txt
+
+  displayAfterChange();
+  document.querySelector('.meme-txt input').value = lineTxt;
+}
+
+
+let onChangeStrokeColor = color => {
+  updateStrokeColor(color);
+  displayAfterChange();
+  document.querySelector('.meme-txt input').focus();
+}
+
+
+let displayAfterChange = () => {
+  let memeStat = getGMeme();
+  let img = getImgById(memeStat.selectedImgId);
+  _displayImgToCanvas(img.url);
+}
+
+
+let onDownloadCanvas = elLink => {
+  const data = gCanvas.toDataURL();
+  elLink.href = data;
+  elLink.download = 'my-meme.jpg';
+}
+
+
+let resizeCanvas = () => {
+  let elContainer = document.querySelector('.canvas-container');
+  gCanvas.width = elContainer.offsetWidth;
+  gCanvas.height = elContainer.offsetHeight;
+}
+
+
+// let onMemeFilter = (that, ev) => {
+//   ev.preventDefault();
+//   console.log(that.value);
+//   renderImgs(that.value);
+// }
---
+++ js/service.js
@@ -0,0 +1,116 @@
+'use strict';
+
+// let gKeywords = { 'happy': 12, 'funny puk': 1 };
+
+let gImgs = [];
+let gMeme = {
+  selectedImgId: undefined,
+  selectedLineIdx: 0,
+  lines: [{
+    txt: '',
+    size: 40,
+    align: 'right',
+    color: '#ffffff',
+    stroke: '#000000',
+    font: 'Impact'
+  }]
+};
+
+
+let _createImg = () => {
+  return {
+    id: undefined,
+    url: undefined,
+    keywords: ['funny']
+  }
+}
+
+
+let createImgs = () => {
+  for (let i = 0; i < 18; i++) {
+    let img = _createImg();
+    img.id = i + 1;
+    img.url = `imgs/meme-imgs/${i + 1}.jpg`;
+
+    if (i <= 3) img.keywords = ['animal', 'funny'];
+    else if (i <= 6) img.keywords = ['kids', 'funny'];
+    else img.keywords = ['funny', 'famous'];
+
+    gImgs.push(img);
+  }
+  return gImgs;
+}
+
+
+ // change to find after
+let getImgById = imgId => {
+  var img;
+  for (var i = 0; i < gImgs.length; i++) {
+    if (gImgs[i].id === imgId) img = gImgs[i];
+  }
+  return img
+}
+
+
+let getGMeme = () => {
+  return gMeme;
+}
+
+
+let addLine = () => {
+  gMeme.lines.push({
+    txt: '',
+    size: 40,
+    align: 'right',
+    color: '#ffffff',
+    stroke: '#000000',
+    font: 'Impact'
+  });
+  gMeme.selectedLineIdx += 1;
+}
+
+
+let updateGMemeId = imgId => {
+  gMeme.selectedImgId = imgId;
+}
+
+
+let updateFont = font => { 
+  gMeme.lines[gMeme.selectedLineIdx].font = font;
+}
+
+
+let updateColor = color => {
+  gMeme.lines[gMeme.selectedLineIdx].color = color;
+}
+
+
+let updateFontSize = operator => {
+  gMeme.lines[gMeme.selectedLineIdx].size += operator;
+}
+
+
+let updateFontAligment = dirName => {
+  gMeme.lines[gMeme.selectedLineIdx].align = dirName;
+}
+
+
+let deleteLine = () => {
+  gMeme.lines.splice(gMeme.selectedLineIdx, 1);
+
+  if (gMeme.lines.length === 0) {
+    gMeme.selectedLineIdx = -1;
+    return undefined;
+  }
+  // else if (gMeme.lines[gMeme.selectedLineIdx] === 0) // check it later
+
+  gMeme.selectedLineIdx = (gMeme.selectedLineIdx === 1) ? 0 : 1;
+
+  return gMeme.lines.length === 0  ? undefined : gMeme.selectedLineIdx;
+}
+
+
+let updateStrokeColor = color => {
+  gMeme.lines[gMeme.selectedLineIdx].stroke = color;
+}
+
